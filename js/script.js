d05da415e1a66729a8e7718e8528386b1e6b4fd8
Iuleasha <iuleasha7@mail.ru>
2020-03-13 22:50:37 +0300

    Защита правки-2

--- js/script.js
+++ js/script.js
@@ -1,68 +1,55 @@
-let writeUsBtn = document.querySelector('.write-us');
-let popup = document.querySelector('.modal-login');
-let close = popup.querySelector('.close-login');
-let form = popup.querySelector('.login-form');
-let login = form.querySelector('[name=login]');
-let email = form.querySelector('[name=email]');
-let letter = form.querySelector('[name=letter]');
-let mapLink = document.querySelector('.map-img');
-let mapPopup = document.querySelector('.modal-map');
-let mapClose = document.querySelector('.map-close');
-let offerBtns = document.querySelectorAll('.offer');
-let deliveryContent = document.querySelectorAll('.delivery-content');
+let writeUsBtn = document.querySelector(".write-us");
+let popup = document.querySelector(".modal-login");
+let close = popup.querySelector(".close-login");
+let form = popup.querySelector(".login-form");
+let login = form.querySelector("[name=login]");
+let email = form.querySelector("[name=email]");
+let letter = form.querySelector("[name=letter]");
+let mapLink = document.querySelector(".map-img");
+let mapPopup = document.querySelector(".modal-map");
+let mapClose = document.querySelector(".map-close");
 
-writeUsBtn.addEventListener('click', function (evt) {
+writeUsBtn.addEventListener("click", function(evt) {
   evt.preventDefault();
-  popup.classList.add('modal-show');
+  popup.classList.remove("modal-exit");
+  popup.classList.add("modal-show");
 });
 
-close.addEventListener('click', function (evt) {
+close.addEventListener("click", function(evt) {
   evt.preventDefault();
-  popup.classList.remove('modal-show');
-  popup.classList.remove('modal-error');
-  popup.classList.add('modal-exit');
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+  popup.classList.add("modal-exit");
 });
 
-form.addEventListener('submit', function (evt) {
+form.addEventListener("submit", function(evt) {
   if (!login.value || !email.value || !letter.value) {
     evt.preventDefault();
-    popup.classList.remove('modal-error');
+    popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add('modal-error');
+    popup.classList.add("modal-error");
   }
 });
 
-window.addEventListener('keydown', function (evt) {
+window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains('modal-show')) {
-      popup.classList.remove('modal-show');
-      popup.classList.remove('modal-error');
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     }
   }
 });
 
-mapLink.addEventListener('click', function (evt) {
+mapLink.addEventListener("click", function(evt) {
   evt.preventDefault();
-  mapPopup.classList.remove('modal-close');
-  mapPopup.classList.add('modal-show');
+  mapPopup.classList.remove("modal-exit");
+  mapPopup.classList.remove("modal-close");
+  mapPopup.classList.add("modal-show");
 });
 
-mapClose.addEventListener('click', function (evt) {
+mapClose.addEventListener("click", function(evt) {
   evt.preventDefault();
-  mapPopup.classList.remove('modal-show');
-  mapPopup.classList.add('modal-exit');
+  mapPopup.classList.remove("modal-show");
+  mapPopup.classList.add("modal-exit");
 });
-
-for (let i = 0; i < offerBtns.length; i++) {
-  offerBtns[i].addEventListener('click', function (evt) {
-    evt.preventDefault();
-    document.querySelector('.offer-active').classList.remove('offer-active');
-    offerBtns[i].classList.add('offer-active');
-
-    document
-      .querySelector('.delivery-content-active')
-      .classList.remove('delivery-content-active');
-    deliveryContent[i].classList.add('delivery-content-active');
-  });
-}
